95eb7c0ab988a04cdb08513ba1ebbb74e94859be
erhimuebru <erhimuebru>
2025-02-06 00:03:55 +0100

    setting boiler-plate for talentsapartments

--- src/sequelize.config.ts
+++ src/sequelize.config.ts
@@ -1,19 +1,22 @@
-import { SequelizeModuleOptions } from '@nestjs/sequelize'; 
+import { SequelizeModuleOptions } from '@nestjs/sequelize';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 export const sequelizeConfig: SequelizeModuleOptions = {
-dialect: 'postgres', 
-  host: process.env.DATABASE_HOST, 
-  port: Number(process.env.DATABASE_PORT), 
-  username: process.env.DATABASE_USERNAME, 
-  password: process.env.DATABASE_PASSWORD, 
-  database: process.env.DATABASE_NAME, 
-  models: [], 
-  logging: false, 
-  ssl: process.env.DATABASE_SSL === 'true', 
+  dialect: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  models: [],
+  logging: false,
+  ssl: process.env.DATABASE_SSL === 'true',
   dialectOptions: {
-    ssl: process.env.DATABASE_SSL === "true" ? { require: true, rejectUnauthorized: false } : false,
+    ssl:
+      process.env.DATABASE_SSL === 'true'
+        ? { require: true, rejectUnauthorized: false }
+        : false,
   },
 };
